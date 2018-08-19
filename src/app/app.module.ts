e477b029a413c82fec60594b82188fbd686a0a17
z-chenet <z.chenet@gmail.com>
2018-08-19 18:58:59 +0100

    getting in sync with demo git page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {Injectable, NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { AppService } from './app.service';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login.component';
-import { AppComponent } from './app.component';
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
+} from '@angular/common/http';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home'},
-  { path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent}
-];
 
 @Injectable()
 export class XhrInterceptor implements HttpInterceptor{
@@ -23,6 +22,11 @@ export class XhrInterceptor implements HttpInterceptor{
     return next.handle(xhr);
   }
 }
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
--- src/main/java/com/example/demo/UiApplication.java
+++ src/main/java/com/example/demo/UiApplication.java
@@ -1,69 +1,70 @@
 package com.example.demo;
 
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 @SpringBootApplication
 @Controller
 public class UiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UiApplication.class, args);
-	}
-
-	@GetMapping("/resource")
-  @ResponseBody
-  public Map<String, Object> home(){
-	  Map<String, Object> model = new HashMap<>();
-	  model.put("id", UUID.randomUUID().toString());
-	  model.put("content", "Hello World");
-	  return model;
-  }
-
-  @RequestMapping("/user")
-  public Principal user(Principal user){
-	  return user;
-  }
-
-  @GetMapping(value = "/{path:[^\\.]*}")
-  public String redirect() {
-    return "forward:/";
-  }
-
-  @Configuration
-  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-      web.ignoring().antMatchers("/*.bundle.*", "/favicon.ico");
+    @GetMapping("/user")
+    @ResponseBody
+    public Principal user(Principal user) {
+        return user;
     }
 
-	  @Override
-    protected void configure(HttpSecurity http) throws Exception {
-      http
-        .httpBasic()
-        .and()
-          .authorizeRequests()
-          .antMatchers("/index.html", "/", "/home", "/login").permitAll()
-          .anyRequest().authenticated()
-        .and().csrf()
-          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    @GetMapping("/resource")
+    @ResponseBody
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
     }
-  }
+
+    @GetMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiApplication.class, args);
+    }
+
+    @Configuration
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers("/*.bundle.*", "/favicon.ico");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                .httpBasic().and()
+                .authorizeRequests()
+                    .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+            // @formatter:on
+        }
+    }
 
 }
